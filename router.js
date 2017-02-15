882998c6d455f6df10bf2bd9ea53e8720e79ce85
Jason <jasonkwong11@gmail.com>
2017-02-15 03:19:46 -0500

    finished

--- router.js
+++ router.js
@@ -1,11 +1,11 @@
 
-const route = (handle, pathname, response, postData) => {
+const route = (handle, pathname, response, request) => {
   console.log(`About to route a request for ${pathname}`);
   if (typeof handle[pathname] === 'function'){
-    handle[pathname](response, postData);
+    handle[pathname](response, request);
   } else {
     console.log(`No request handlers found for' + ${pathname}`);
-    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.writeHead(404, {'Content-Type': 'text/html'});
     response.write('404 Not found');
     response.end();
   }
--- server.js
+++ server.js
@@ -4,20 +4,9 @@ var formidable = require('formidable');
 
 const start = (route, handle) => {
   function onRequest(request, response) {
-    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log(`Request for ${pathname} received.`);
-
-    request.setEncoding('utf8');
-
-    request.addListener('data', (postDataChunk) => {
-      postData += postDataChunk;
-      console.log('Received POST data chunk: ' + postDataChunk);
-    });
-
-    request.addListener('end', () => {
-      route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888)
